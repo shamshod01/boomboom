c2cbf4237b51248cec85f9f9b2acac420c7902a5
shamshod01 <realshamshod01@gmail.com>
2024-05-28 17:56:23 +0900

    last

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,14 @@ import React from 'react';
 import FlipImage from './FlipImage';
 import frontImage from './usd-front.png';
 import backImage from './back.png';
+import './App.css';
 
 const App: React.FC = () => {
-  return (
-      <div className="App">
-        <FlipImage frontImage={frontImage} backImage={backImage} />
-      </div>
-  );
+    return (
+        <div className="App">
+            <FlipImage frontImage={frontImage} backImage={backImage} />
+        </div>
+    );
 };
 
 export default App;
--- src/FlipImage.tsx
+++ src/FlipImage.tsx
@@ -1,6 +1,7 @@
 // src/FlipImage.tsx
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './FlipImage.css';
+
 interface FlipImageProps {
     frontImage: string;
     backImage: string;
@@ -8,23 +9,32 @@ interface FlipImageProps {
 
 const FlipImage: React.FC<FlipImageProps> = ({ frontImage, backImage }) => {
     const [isFlipped, setIsFlipped] = useState(false);
-    const [flipSound, setFlipSound] = useState<HTMLAudioElement | null>(null);
+    const [flipCount, setFlipCount] = useState(0);
+    const [isMoving, setIsMoving] = useState(false);
+    const flipSound = useRef<HTMLAudioElement | null>(null);
 
     useEffect(() => {
         // Initialize the audio element once the component mounts
-        const audio = new Audio('/sound.mp3');
-        setFlipSound(audio);
+        flipSound.current = new Audio(process.env.PUBLIC_URL + '/sound.mp3');
     }, []);
 
     const handleFlip = () => {
-        if (flipSound) {
-            flipSound.play();
+        if (flipSound.current) {
+            flipSound.current.play();
         }
-        setIsFlipped(!isFlipped);
+        setIsFlipped(true);
+        setIsMoving(true);
+        setTimeout(() => {
+            setIsFlipped(false);
+            setIsMoving(false);
+        }, 2000);
     };
 
     return (
-        <div className={`flip-container ${isFlipped ? 'flipped' : ''}`} onClick={handleFlip}>
+        <div
+            className={`flip-container ${isFlipped ? 'flipped' : ''} ${isMoving ? 'moving-up' : ''}`}
+            onClick={handleFlip}
+        >
             <div className="flipper">
                 <img className="front" src={frontImage} alt="Front" />
                 <img className="back" src={backImage} alt="Back" />
